e0681cb8870c1092cd3a61df08fbcf2f2a5e0b72
Sahil Singla <singla@Singlas-Macbook.local>
2026-09-17 07:36:29 +0530

    polymorphism

--- 06_oops/05_solution.py
+++ 06_oops/05_solution.py
@@ -1,5 +1,7 @@
 # Polymorphism - One interface/method name, but different behavior depending on the object.
 
+# Method overriding - Method overriding is one common way to achieve polymorphism.
+
 # Demonstrate polymorphism by defining a method fuel_type in both Car and ElectricCar classes, but with different behaviors
 
 class Car:
@@ -24,11 +26,55 @@ class ElectricCar(Car):
     def fuel_type(self):
         return "Electric Charge"
 
+class HydrogenCar(Car):
+    def fuel_type(self):
+        return "Hydrogen"
+
 
 my_car = Car("Tata", "Safari")
 print(my_car.car_details())
-print(my_car.fuel_type())
+print("Fuel Type:", my_car.fuel_type())
 
 my_electric_car = ElectricCar("Tesla", "Model S", "85kWh")
 print(my_electric_car.electric_car_details())
-print(my_electric_car.fuel_type())
+print("Fuel Type:", my_electric_car.fuel_type())
+
+my_hydrogen_car = HydrogenCar("Toyota", "Mirai")
+print(my_hydrogen_car.car_details())
+print("Fuel Type:", my_hydrogen_car.fuel_type())
+
+cars = [my_car, my_electric_car, my_hydrogen_car]
+
+for car in cars:
+    print("Fuel Type:",car.fuel_type())
+
+
+cars = [
+    Car("BMW", "X5"),
+    ElectricCar("MG", "Cyberster", "375kWh"),
+    HydrogenCar("Hyundai", "Nexo")
+]
+
+for car in cars:
+    print("Fuel:", car.fuel_type())
+
+
+
+
+# Polymorphism without inheritance
+class Dog:
+    def sound(self):
+        return "Bark"
+
+class Cat:
+    def sound(self):
+        return "Meow"
+
+class Cow:
+    def sound(self):
+        return "Moo"
+
+animals = [Dog(), Cat(), Cow()]
+
+for animal in animals:
+    print(animal.sound())
