8f8f69f98903f708868be6ea266a98486a73df61
Sahil Singla <singla@Singlas-Macbook.local>
2026-09-17 07:07:39 +0530

    Encapsulation and Getter and Setter)
    dsfs

--- 06_oops/04_solution.py
+++ 06_oops/04_solution.py
@@ -1,4 +1,4 @@
-# Encapsulation - It is a fundamental concept in object-oriented programming (OOP) that restricts direct access to an object's attributes and methods. It allows for controlled access through public methods, ensuring that the internal state of an object is protected from unintended interference and misuse.
+# Encapsulation - Encapsulation means making variables private.
 
 # Problem: Modify the Car Class to encapsulate the brand attribute, making it private and provide a getter method to access it.
 
@@ -12,8 +12,76 @@ class Car:
 
     def full_name(self):
             return f"Brand: {self.__brand} and Model: {self.model}"
-
+    
 my_car = Car("Toyota", "Corolla")
 # print(my_car.__brand) # This will raise an AttributeError
 print(my_car.get_brand())
 print(my_car.full_name())
+
+
+
+
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__balance:
+            self.__balance -= amount
+
+    def get_balance(self):
+        return self.__balance
+
+account = BankAccount(10000)
+account.deposit(5000)
+account.withdraw(2000)
+account.withdraw(20000) # this dorsn't executes here, our condition doesn't satisfies 
+print("Amount:", account.get_balance())
+
+
+
+
+
+
+# Getter (get/read data) and Setter(changes/updates data)
+# Method 1
+class Person1:
+    def __init__(self, age):
+        self.__age = age
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, age):
+        if age >= 0:
+            self.__age = age
+
+person1 = Person1(25)
+print(person1.get_age())
+person1.set_age(30)
+print(person1.get_age())
+
+
+
+
+#  Method 2
+class Person2:
+    def __init__(self, age):
+        self.__age = age
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if value >= 0:
+            self.__age = value
+person2 = Person2(20)
+print(person2.age)
+person2.age = 25
+print(person2.age)
